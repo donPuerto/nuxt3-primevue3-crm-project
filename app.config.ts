c7071dacfcb2f0a3a8dbc515892d83a9fdcfc9a9
donPuerto <don.puerto@hotmail.com>
2023-09-29 23:28:46 +0800

    Remove cached files

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineAppConfig({
-  title: 'OneDP App',
-  description: 'An app for CRM',
-  fullName: 'Don Puerto',
-  username: 'donpuerto',
-  email: 'don.puerto@hotmail.com',
-  github: 'https://github.com/donPuerto',
-})
--- layouts/empty.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="layout-empty">
-    <Toast position="top-right" />
-    <slot />
-  </div>
-</template>
