ef749827d0bc5c6f59152bb898641ceeacbce401
donPuerto <don.puerto@hotmail.com>
2023-09-29 23:18:54 +0800

    securing assets

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -8,25 +8,31 @@ const formFields = ref([
   {
     id: 'email1',
     label: 'Email',
-    type: 'InputText',
-    labelClass: 'block text-900 font-medium mb-2',
-    inputClass: 'w-full mb-3',
+    type: InputText,
     value: '',
+    size: 'medium',
+    placeholder: 'Email',
+    icon: 'pi-envelope',
+    ariaDescribedBy: 'email-help',
+    small: 'Enter your email address for account recovery.',
   },
   {
     id: 'password1',
     label: 'Password',
-    type: 'Password',
-    labelClass: 'block text-900 font-medium mb-2',
-    inputClass: 'w-full mb-3',
+    type: Password,
     value: '',
+    size: 'medium',
+    placeholder: 'Password',
+    icon: 'pi-lock',
+    ariaDescribedBy: 'password-help',
+    small: 'Enter your password to access your account.',
   },
 ])
 
 function handleSubmit() {
   const formData = {}
 
-  for (const field of formFields)
+  for (const field of formFields.value)
     formData[field.id] = field.value
 
   // Now you have all the form data in the formData object
@@ -34,13 +40,35 @@ function handleSubmit() {
 
   // You can perform your form submission or other logic here
 }
+
+function getSizeClass(size) {
+  const sizeClassMap = {
+    small: 'p-inputtext-sm',
+    medium: '', // Default size, no additional class needed
+    large: 'p-inputtext-lg',
+  }
+
+  return sizeClassMap[size] || ''
+}
 </script>
 
 <template>
-  <div>
-    <div v-for="field in formFields" :key="field.id">
-      <component :is="field.type" v-model="field.value" :class="field.inputClass" />
-    </div>
-    <Button label="Submit" @click="handleSubmit" />
+  <div class="p-8">
+    <form @submit.prevent="handleSubmit">
+      <div v-for="field in formFields" :key="field.id">
+        <div class="flex flex-column pb-2">
+          <label :for="field.id">{{ field.label }}</label>
+          <div class="p-inputgroup">
+            <span v-if="field.icon" class="p-inputgroup-addon p-input-icon-left">
+              <i class="pi" :class="[field.icon]" />
+            </span>
+            <InputText :id="field.id" v-model="field.value" :class="getSizeClass(field.size)"
+              :placeholder="field.placeholder" :aria-describedby="field.ariaDescribedBy" />
+          </div>
+          <small v-if="field.small" :id="field.ariaDescribedBy">{{ field.small }}</small>
+        </div>
+      </div>
+      <Button label="Submit" type="submit" :class="getSizeClass('medium')" />
+    </form>
   </div>
 </template>
